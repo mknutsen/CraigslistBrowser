6e44b5fee7b1c029b99d9bdd7f48a997e6715119
Max Knutsen <mknutsen99@gmail.com>
2016-01-11 23:57:38 -0500

    found out that the regex thats splitting the descritpions up has a bunch of bugs: 1) theres sometimes a lot of other text in that area that isn't a description 2) the regex isn't splitting stuff up right so the list is totally jumbled
    i made  a note in the code that it doesn't work so i dont get confused later

---
+++ src/mknutsen/craiglist/parser/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package accepts a location and processes craigslist ads nearby.
+ * depends on jsoup 1.8.3 (but i want to update this project to gradle so hopefully that won't be a problem)
+ */
+package mknutsen.craiglist.parser;
