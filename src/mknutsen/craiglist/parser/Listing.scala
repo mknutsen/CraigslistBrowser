dc471c100712c147d94c2655b6bf82783aacc745
Max Knutsen <mknutsen99@gmail.com>
2016-01-13 22:27:55 -0500

    fixed the parsing of the description pane by checking the html tags instead of looking for the end of sentances. the issue is when someone puts in just the title of the thing in bold in the description pane, the program gets super duper confused. ill have to program an explicit case for that later.

--- src/mknutsen/craiglist/parser/Listing.scala
+++ src/mknutsen/craiglist/parser/Listing.scala
@@ -4,65 +4,61 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.select.Elements
 
-//import scala.collection.JavaConversions._
-
-import scala.collection.JavaConversions._
 import scala.collection.mutable
 
+//import scala.collection.JavaConversions._
+import scala.collection.JavaConversions._
+
 /**
- * Created by mknutsen on 1/11/16.
- */
+  * Created by mknutsen on 1/11/16.
+  */
 class Listing(url: String, document: Document) {
   /**
-   * Titles is in the postingtitle class
-   */
+    * Titles is in the postingtitle class
+    */
   private val title = document.select(".postingtitle").text()
 
   /**
-   * the body has the ID postingboy
-   */
+    * the body has the ID postingbody
+    */
   private val postBody = document.select("#postingbody").text()
 
   /**
-   * Images url is located somewhere in the iw oneimage class, ill parse that later
-   */
+    * Images url is located somewhere in the iw oneimage class, ill parse that later
+    */
   private val imageLocation = document.select(".iw oneimage")
 
   /**
-   * description is in attrgroup (stuff like condition and size is specified here)
-   * It gets parsed into a hashtable of key being description type (condition) and value being description (new)
-   * **this doesn't work yet **
-   */
-  private val description = document.select(".attrgroup").text()
-  private val descriptionList = description.split("[ :]+")
-  private val descriptionTable = new mutable.HashMap[String, String]()
-  for (i <- 0 to descriptionList.length - 2 by 2) {
-    //    println(descriptionList(i) + " " + descriptionList(i + 1))
-    descriptionTable.put(descriptionList(i), descriptionList(i + 1))
+    * description is in attrgroup (stuff like condition and size is specified here)
+    * It gets parsed into a hashtable of key being description type (condition) and value being description (new)
+    * **this doesn't work yet **
+    */
+  val descriptionTable = new mutable.HashMap[String, String]()
+  private var description = document.select(".attrgroup").toString().toLowerCase()
+  while (description.indexOf("<span>") > -1 && description.indexOf("</b>") > -1) {
+    val key = Listing.extractStringBetweenIndicators("<span>", ":", description)
+    val value = Listing.extractStringBetweenIndicators("<b>", "</b>", description)
+    descriptionTable.put(key, value)
+    val newLoc = description.indexOf("</b>") + "</b>".length
+    description = description.substring(newLoc)
   }
   private val textDocument = document.text()
 
   /**
-   * Parses out the cost of the object
-   */
-  private var documentStringCounter = textDocument.indexOf('$') + 1
-  private var amount = 0
-  private var currentChar = textDocument.charAt(documentStringCounter)
-  private var priceOnPage = false
-  while (documentStringCounter < textDocument.length && currentChar >= 48 && currentChar <= 57) {
-    priceOnPage = true
-    amount *= 10
-    amount += Integer.parseInt(textDocument.charAt(documentStringCounter) + "")
-    documentStringCounter += 1
-    currentChar = textDocument.charAt(documentStringCounter)
-  }
+    * Parses out the cost of the object
+    */
+  private val amount = Listing.extractStringAfterIndicator('0', '9', textDocument, "$")
   /**
-   * cost of the item is $-1 if the price wasn't actually listed
-   */
-  private val itemCost = if (priceOnPage) amount else -1
+    * cost of the item is $-1 if the price wasn't actually listed
+    */
+  private val itemCost = if (amount.length() > 0) Integer.parseInt(amount) else -1
 
   if (itemCost == -1) {
     System.err.println(url)
+  }
+
+  private def parseDescription(descriptionList: Array[String]) = {
+    for (i <- 0 to descriptionList.length - 2 by 2) yield descriptionList(i) -> descriptionList(i + 1)
   }
 
 
@@ -70,7 +66,25 @@ class Listing(url: String, document: Document) {
 }
 
 object Listing {
+  def extractStringAfterIndicator(low: Character, high: Character, str: String, indicator: String): String = {
+    var returnVal = ""
+    var start = str.indexOf(indicator) + indicator.length
+    val strArray = str.toCharArray
+    while (strArray(start) <= high && strArray(start) >= low) {
+      returnVal += strArray(start)
+      start += 1
+    }
+    return returnVal
+  }
 
+  def extractStringBetweenIndicators(startString: String, endString: String, str: String): String = {
+    val startLoc = str.indexOf(startString)
+    if (startLoc < 0) {
+      return ""
+    }
+    val strStartingPoint = str.substring(startLoc + startString.length)
+    return strStartingPoint.substring(0, strStartingPoint.indexOf(endString))
+  }
 
   def getElements(url: String, selector: String): Elements = {
     Jsoup.connect(url).get().select(selector)
--- src/mknutsen/craiglist/parser/Parser.scala
+++ src/mknutsen/craiglist/parser/Parser.scala
@@ -3,17 +3,17 @@ package mknutsen.craiglist.parser
 import org.jsoup.Jsoup
 
 /**
- * Created by mknutsen on 1/10/16.
- */
+  * Created by mknutsen on 1/10/16.
+  */
 object Parser {
   var baseURL = ".craigslist.org"
   val searchStart = "/search/sss?s"
 
   /**
-   *
-   * @param args
+    *
+    * @param args
    * [city in a string like washingtondc for washingtondc.craigslist.com, number of pages to look through]
-   */
+    */
   def main(args: Array[String]): Unit = {
     baseURL = "http://" + args(0) + baseURL
     val numPages = if (args.length >= 2) Integer.parseInt(args(1)) else 1
@@ -22,15 +22,15 @@ object Parser {
       val tempListings = getListings(baseURL + searchStart + i * 100)
       listings = tempListings ::: listings
     }
-    println(listings.length)
+    println("number of listings: " + listings.length)
     for (listing <- listings) println(listing)
   }
 
   def getListings(url: String): List[Listing] = {
     val elems = Listing.getElements(url, ".i")
-    //    while (elems.length > 1) {
-    //      elems.remove(1)
-    //    }
+    while (elems.size() > 15) {
+      elems.remove(1)
+    }
     val links = Listing.getUrls(elems)
     for (link <- links) yield new Listing(baseURL + link, Jsoup.connect(baseURL + link).get())
   }
