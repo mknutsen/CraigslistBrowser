5f185bd7242df2066356e1eeec535dcf179c6bbc
Max Knutsen <mknutsen99@gmail.com>
2016-01-18 21:58:33 -0500

    recognizes dead links but doesnt instantiate dead links properly yet

--- src/main/scala/mknutsen/craiglist/parser/ListingLoader.scala
+++ src/main/scala/mknutsen/craiglist/parser/ListingLoader.scala
@@ -5,25 +5,30 @@ import org.jsoup.Jsoup
 import scala.io.Source
 
 /**
-  * Created by mknutsen on 1/15/16.
-  */
+	* Created by mknutsen on 1/15/16.
+	*/
 object ListingLoader {
-  def processLineSegment(lineSegments: Array[String]): Listing = {
-    return new Listing(lineSegments(2), Jsoup.connect(lineSegments(2)).get())
-  }
+	def processLineSegment ( lineSegments : Array[ String ] ) : Listing = {
+		val document = Jsoup.connect ( lineSegments ( 2 ) ).get ( )
+		if ( document.toString.indexOf ( "Not Found" ) > -1 ) {
+			return new Listing ( lineSegments ( 2 ), Jsoup.connect ( lineSegments ( 2 ) ).get ( ) )
+		} else {
+			return new Listing ( lineSegments ( 2 ), true, "", "", null, null, -1, "" ) // to implement later
+		}
+	}
 
-  def serializeListing(listing: Listing): String = {
-    if (listing.getIsDead()) "" else listing.getTitle() + ";" + listing.getURL()
-  }
+	def serializeListing ( listing : Listing ) : String = {
+		if ( listing.getIsDead ( ) ) "" else listing.getTitle ( ) + ";" + listing.getURL ( )
+	}
 
-  def main(args: Array[String]): Unit = {
-    var listings = List[Listing]()
-    for (line <- Source.fromFile(args(0)).getLines()) {
-      val lineSegments = line.split(";")
-      val newListing = ListingLoader.processLineSegment(lineSegments)
-      println(newListing)
-      listings = newListing :: listings
-    }
-    print(listings)
-  }
+	def main ( args : Array[ String ] ) : Unit = {
+		var listings = List [ Listing ]( )
+		for ( line <- Source.fromFile ( args ( 0 ) ).getLines ( ) ) {
+			val lineSegments = line.split ( ";" )
+			val newListing = ListingLoader.processLineSegment ( lineSegments )
+			println ( newListing )
+			listings = newListing :: listings
+		}
+		print ( listings )
+	}
 }
