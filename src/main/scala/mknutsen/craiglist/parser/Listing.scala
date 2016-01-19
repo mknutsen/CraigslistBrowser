4917079549ee9481d311e7d3c7098be79c755b73
Max Knutsen <mknutsen99@gmail.com>
2016-01-18 21:35:33 -0500

    listing now scrapes for date and also the code has been reformatted and looks way more organized

--- src/main/scala/mknutsen/craiglist/parser/Listing.scala
+++ src/main/scala/mknutsen/craiglist/parser/Listing.scala
@@ -8,113 +8,119 @@ import org.jsoup.select.Elements
 import scala.collection.JavaConversions._
 
 /**
-  * Created by mknutsen on 1/11/16.
-  */
-class Listing(url: String, document: Document) {
-  private val isDead = document.text().indexOf("Page Not Found") != -1
-  /**
-    * Titles is in the postingtitle class
-    */
-  private val title = if (isDead) "" else document.select(".postingtitle").text()
+	* @param url
+	* url of the listing
+	* @param isDead
+	* is the listing still up
+	* @param title
+	* title of the listing
+	* @param postBody
+	* body of the posting
+	* @param imageLocation
+	* image information to be parsed later
+	* @param descriptionTable
+	* table of descriptions (condition->excellent, etc)
+	* @param cost
+	* how much this cost
+	*/
+class Listing ( url : String, isDead : Boolean, title : String, postBody : String, imageLocation : Elements,
+								descriptionTable : scala.collection.mutable.HashMap[ String, String ], cost : Int,
+								datePosted : String ) {
 
-  /**
-    * the body has the ID postingbody
-    */
-  private val postBody = if (isDead) "" else document.select("#postingbody").text()
-
-  /**
-    * Images url is located somewhere in the iw oneimage class, ill parse that later
-    */
-  private val imageLocation = if (isDead) "" else document.select(".iw oneimage")
-
-  /**
-    * description is in attrgroup (stuff like condition and size is specified here)
-    * It gets parsed into a hashtable of key being description type (condition) and value being description (new)
-    * **this doesn't work yet **
-    */
-  private val description = if (isDead) new Elements() else document.select(".attrgroup")
-  private val descriptionTable = parseDescription(description)
-  private val textDocument = if (isDead) "" else document.text()
-
-  /**
-    * Parses out the cost of the object
-    */
-  private val amount = Listing.extractStringAfterIndicator('0', '9', textDocument, "$")
-
-  /**
-    * cost of the item is $-1 if the price wasn't actually listed
-    */
-  private val itemCost = if (amount.length() > 0) Integer.parseInt(amount) else -1
-
-  if (itemCost == -1) {
-    System.err.println(url)
-  }
-
-  private def parseDescription(descriptionList: Elements): scala.collection.mutable.HashMap[String, String] = {
-    var descriptionText = if (isDead) "" else descriptionList.toString().toLowerCase()
-    val descriptionTable = new scala.collection.mutable.HashMap[String, String]()
-    while (descriptionText.indexOf("<span>") > -1 && descriptionText.indexOf("</b>") > -1) {
-      if (descriptionText.indexOf("<span>") + "<span>".length != descriptionText.indexOf("<b>")) {
-        val key = Listing.extractStringBetweenIndicators("<span>", ":", descriptionText)
-        val value = Listing.extractStringBetweenIndicators("<b>", "</b>", descriptionText)
-        descriptionTable.put(key, value)
-        val newLoc = descriptionText.indexOf("</b>") + "</b>".length
-        descriptionText = descriptionText.substring(newLoc)
-      } else{
-        descriptionText = descriptionText.substring(descriptionText.indexOf("</span>") + "</span>".length)
-      }
-    }
-    return descriptionTable
-  }
+	/**
+		* @param url
+		* @param document
+		*/
+	def this ( url : String, document : Document ) = {
+		this ( url, false, document.select ( ".postingtitle" ).text ( ), document.select ( "#postingbody" ).text ( ),
+					 document.select
+					 ( ".iw oneimage" ), Listing.parseDescription ( document.select ( ".attrgroup" ) ),
+					 Listing.parsePriceFromString ( Listing
+																						.extractStringAfterIndicator ( '0', '9', document.text ( ), "$" ) ),
+					 Listing.extractStringBetweenIndicators ( "datetime=\"", "\"",
+																										document.select ( "#display-date" ).toString ) )
+		println ( document.select ( "#display-date" ) )
+	}
 
 
-  def getTitle() = title
+	def getTitle ( ) = title
 
-  def getPrice() = itemCost
+	def getPrice ( ) = cost
 
-  def getURL() = url
+	def getURL ( ) = url
 
-  def getIsDead() = isDead
+	def getIsDead ( ) = isDead
 
-  override def toString = {
-    if (isDead) {
-      "Link is dead at " + url
-    } else {
-      "Listed: " + title + " for $" + itemCost + " at " + url + "  " + descriptionTable
-    }
-  }
+	override def toString = {
+		if ( isDead ) {
+			"Link is dead at " + url
+		} else {
+			"Listed: " + title + " for $" + cost + " at " + url + "  " + descriptionTable + " posted on " + datePosted
+		}
+	}
 }
 
 object Listing {
-  def extractStringAfterIndicator(low: Character, high: Character, str: String, indicator: String): String = {
-    var returnVal = ""
-    var start = str.indexOf(indicator) + indicator.length
-    val strArray = str.toCharArray
-    while (start < strArray.length && strArray(start) <= high && strArray(start) >= low) {
-      returnVal += strArray(start)
-      start += 1
-    }
-    return returnVal
-  }
+	def parseDescription ( descriptionList : Elements ) : scala.collection.mutable.HashMap[ String, String ] = {
+		var descriptionText = if ( descriptionList == null ) "" else descriptionList.toString ( ).toLowerCase ( )
+		val descriptionTable = new scala.collection.mutable.HashMap[ String, String ]( )
+		while ( descriptionText.indexOf ( "<span>" ) > -1 && descriptionText.indexOf ( "</b>" ) > -1 ) {
+			if ( descriptionText.indexOf ( "<span>" ) + "<span>".length != descriptionText.indexOf ( "<b>" ) ) {
+				val key = Listing.extractStringBetweenIndicators ( "<span>", ":", descriptionText )
+				val value = Listing.extractStringBetweenIndicators ( "<b>", "</b>", descriptionText )
+				descriptionTable.put ( key, value )
+				val newLoc = descriptionText.indexOf ( "</b>" ) + "</b>".length
+				descriptionText = descriptionText.substring ( newLoc )
+			} else {
+				descriptionText = descriptionText.substring ( descriptionText.indexOf ( "</span>" ) + "</span>".length )
+			}
+		}
+		return descriptionTable
+	}
 
-  def extractStringBetweenIndicators(startString: String, endString: String, str: String): String = {
-    val startLoc = str.indexOf(startString)
-    if (startLoc < 0) {
-      return ""
-    }
-    val strStartingPoint = str.substring(startLoc + startString.length)
-    return strStartingPoint.substring(0, strStartingPoint.indexOf(endString))
-  }
+	def extractStringAfterIndicator ( low : Character, high : Character, str : String, indicator : String ) : String = {
+		var returnVal = ""
+		var start = str.indexOf ( indicator ) + indicator.length
+		val strArray = str.toCharArray
+		while ( start < strArray.length && strArray ( start ) <= high && strArray ( start ) >= low ) {
+			returnVal += strArray ( start )
+			start += 1
+		}
+		return returnVal
+	}
 
-  def getElements(url: String, selector: String): Elements = {
-    Jsoup.connect(url).get().select(selector)
-  }
+	def extractStringBetweenIndicators ( startString : String, endString : String, str : String ) : String = {
+		val startLoc = str.indexOf ( startString )
+		println(startString)
+		if ( startLoc < 0 ) {
+			return ""
+		}
+		val strStartingPoint = str.substring ( startLoc + startString.length )
+		println ( strStartingPoint )
+		println ( strStartingPoint.indexOf ( endString ) )
+		return strStartingPoint.substring ( 0, strStartingPoint.indexOf ( endString ) )
+	}
 
-  def getUrls(elems: Elements): List[String] = {
-    (for (elem <- elems) yield elem.attr("href") mkString) toList
-  }
+	def getElements ( url : String, selector : String ) : Elements = {
+		Jsoup.connect ( url ).get ( ).select ( selector )
+	}
 
-  def elementToString(elems: List[Elements]): List[String] = {
-    for (elem <- elems) yield elem.text mkString
-  }
+	def getUrls ( elems : Elements ) : List[ String ] = {
+		( for ( elem <- elems ) yield elem.attr ( "href" ) mkString ) toList
+	}
+
+	def elementToString ( elems : List[ Elements ] ) : List[ String ] = {
+		for ( elem <- elems ) yield elem.text mkString
+	}
+
+	def parsePriceFromString ( amount : String ) : Int = {
+
+
+		/**
+			* cost of the item is $-1 if the price wasn't actually listed
+			*/
+		val itemCost = if ( amount.length ( ) > 0 ) Integer.parseInt ( amount ) else -1
+
+		return itemCost
+	}
 }
